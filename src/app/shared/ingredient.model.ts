8c81d2faf4bad14b599d42a9c9dadc1b7ed7b0b0
Saptarshi De <saptarshide2013@gmail.com>
2020-07-19 14:58:12 +0530

    Section 3 -- Basic Setup Complete

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,18 @@
+export class Ingredient{
+
+  /*  public name: string;
+    public amount: number;
+
+    constructor(name: string, amount: number){
+        this.name = name;
+        this.amount = amount;
+    }
+
+    While declaring a model class, we can take either of the two approaches if we decide to pass all the parameters
+    via constructor.
+    */
+
+    constructor(public name: string, public amount: number){
+
+    }
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model'
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients : Ingredient[] = [
+    new Ingredient("Apples",10),
+    new Ingredient("Mangoes",5)
+  ];
 
   constructor() { }
 
